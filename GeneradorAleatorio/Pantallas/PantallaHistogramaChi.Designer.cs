939d9bc12fd3452ba358dfe0ed19d377bfdb5ad3
Natalia Barrionuevo <barrionuevonatalia1998@gmail.com>
2023-04-15 16:22:22 -0300

    cambio minimo en el diseño pantalla histograma

--- GeneradorAleatorio/Pantallas/PantallaHistogramaChi.Designer.cs
+++ GeneradorAleatorio/Pantallas/PantallaHistogramaChi.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.DataGVCHI = new System.Windows.Forms.DataGridView();
             this.LimInf = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LimSup = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -93,17 +93,17 @@
             this.Histograma1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea4.Name = "ChartArea1";
-            this.Histograma1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.Histograma1.Legends.Add(legend4);
+            chartArea2.Name = "ChartArea1";
+            this.Histograma1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.Histograma1.Legends.Add(legend2);
             this.Histograma1.Location = new System.Drawing.Point(12, 314);
             this.Histograma1.Name = "Histograma1";
             this.Histograma1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Intervalos";
-            this.Histograma1.Series.Add(series4);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Intervalos";
+            this.Histograma1.Series.Add(series2);
             this.Histograma1.Size = new System.Drawing.Size(1336, 372);
             this.Histograma1.TabIndex = 1;
             this.Histograma1.Text = "Histograma de los datos generados";
@@ -160,7 +160,7 @@
             // 
             this.lblAceptacion.AutoSize = true;
             this.lblAceptacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAceptacion.Location = new System.Drawing.Point(993, 143);
+            this.lblAceptacion.Location = new System.Drawing.Point(862, 138);
             this.lblAceptacion.Name = "lblAceptacion";
             this.lblAceptacion.Size = new System.Drawing.Size(52, 17);
             this.lblAceptacion.TabIndex = 7;
